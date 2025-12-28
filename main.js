040f7452c329ba438c33ff183329a5bbb36e1bcd
Hoyoung Chung / 정호영 <235833667+cxo-ca@users.noreply.github.com>
2025-12-29 00:22:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const button = document.getElementById("introBtn");
+const message = document.getElementById("introMsg");
 
+button.addEventListener("click", () => {
+  message.textContent =
+    "실제 업무라는 책임감을 가지고, 작은 과제라도 끝까지 완성하는 개발자가 되겠습니다.";
+});
